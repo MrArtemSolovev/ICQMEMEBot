2bbcaca30e53b3953c129f4940b8405365d452ee
MrArtemSolovev <artemsolovev1309@yandex.ru>
2020-04-21 11:33:44 +0300

    update main()

--- icq_meme_bot.py
+++ icq_meme_bot.py
@@ -41,6 +41,7 @@ def meme_sender(bot, event):
         article_id_sql.article_id_insert(post_raw)
         bot.send_text(chat_id=event.from_chat , text=post_raw[1] +'\n\n'+ post_raw[2]+'\n\n'+'Источник: '+post_raw[3])
 
+
 def main():
     bot.dispatcher.add_handler(UnknownCommandHandler(callback=unknown_command_cb))
     bot.dispatcher.add_handler(StartCommandHandler(callback=start_command))
@@ -48,6 +49,7 @@ def main():
     bot.dispatcher.add_handler(CommandHandler(command="meme", callback=meme_sender))
     bot.start_polling()
     bot.idle()
+
 
 if __name__ == "__main__":
     main()
